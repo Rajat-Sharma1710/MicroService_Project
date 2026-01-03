0cd12e15d3d8874cbddaa3673eae32e874011782
rajatSharma <rajatsharma@Rajats-MacBook-Air.local>
2026-01-03 19:56:03 +0530

    Create employee service

---
+++ Employee/src/main/java/com/employee/demo/repository/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.employee.demo.repository;
+
+import com.employee.demo.model.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    //
+}
